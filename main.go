d3198205c6ce24002ef71673ee79a22b8c6e41a5
ralph <ralphxwz@gmail.com>
2023-10-14 15:54:51 +0800

    update

--- main.go
+++ main.go
@@ -24,6 +24,6 @@ func main() {
 
 	// let fib = func(x){if(x==0){return 0}else{if(x==1){return 1;}else{return fib(x-1) + fib(x-2);}}};
 	// compiler.Start(os.Stdin, os.Stdout)
-	// interpretor.Start(in, os.Stdout)
 	interpretor.Start(in, os.Stdout)
+	// interpretor.Start(in, os.Stdout)
 }
