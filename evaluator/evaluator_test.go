dec405689987db3fb65d1f75aa05fdeaa3690781
ralph <ralphxwz@gmail.com>
2023-10-26 15:47:18 +0800

    add test for assignment

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -249,6 +249,8 @@ func TestLetStatements(t *testing.T) {
 		{"let a = 5 * 5; a;", 25},
 		{"let a = 5; let b = a; b;", 5},
 		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+        {"let a = 0; a += a -= 2", -4},
+        {"let a = 10; a += a -= a *= a /= a %= 3", 0},
 	}
 
 	for _, tt := range tests {
